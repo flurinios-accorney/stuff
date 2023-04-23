d913af8c7b6a2cc912a81f5ded20d7590bc5bc6a
flurinios-accorney <78545028+flurinios-accorney@users.noreply.github.com>
2023-04-23 13:59:05 +0200

    Update spellcards.lua

--- spellcards.lua
+++ spellcards.lua
@@ -190,7 +190,7 @@ local function newSpellSign(text, element)
 	end
 	
 	local clone = cachedUI:Clone()
-	clone.imageLabel2.ImageColor3 = elementColors[element] or Color3.fromRGB(255,255,255)
+	clone.imageLabel2.ImageColor3 = elementColors[element] and elementColors[element] or Color3.fromRGB(255,255,255)
 	clone.textLabel.Text = '<i>'..text..'</i>'
 
 	return {
@@ -200,7 +200,7 @@ local function newSpellSign(text, element)
 		line2 = clone.line.line2,
 		line3 = clone.line.line2.line3,
 		imageLabel = clone.imageLabel,
-		imageLabel2 = clone.ImageLabel2,
+		imageLabel2 = clone.imageLabel2,
 		scale = clone.scale
 	}
 end
