a0a666c6a88578da1758486475019e5c9f64f731
flurinios-accorney <78545028+flurinios-accorney@users.noreply.github.com>
2023-04-25 19:04:24 +0200

    Update spellcards.lua

--- spellcards.lua
+++ spellcards.lua
@@ -43,6 +43,179 @@ local backpack = player:WaitForChild("Backpack", math.huge) or player.Backpack:W
 local lastSpell
 local registered = {}
 
+local mantraStars = {
+	-- flamecharm
+	-- combat
+	["Fire Blade"] = 0,
+	["Flame Repulsion"] = 0,
+	["Burning Servants"] = 0,
+	["Fire Gun"] = 0,
+	["Flame Grab"] = 0,
+	["Flame Blind"] = 0,
+	["Fire Palm"] = 1,
+	["Fire Eruption"] = 1,
+	["Fire Forge"] = 1,
+	["Rising Flame"] = 2,
+	["Flame Assault"] = 2,
+	["Ash Slam"] = 3,
+	-- mobility
+	["Flame Leap"] = 2,
+	-- support
+	["Flame of Denial"] = 0,
+	["Graceful Flame"] = 1,
+	["Flame Wisp"] = 2,
+	
+	-- thundercall
+	-- combat
+	["Lightning Blade"] = 0,
+	["Jolt Grab"] = 0,
+	["Electro Carve"] = 0,
+	["Lightning Beam"] = 0,
+	["Lightning Impact"] = 1,
+	["Lightning Clones"] = 1,
+	["Lightning Strike"] = 1,
+	["Thunder Kick"] = 1,
+	["Storm Blades"] = 1,
+	["Grand Javelin"] = 2,
+	["Bolt Piercer"] = 2,
+	["Rising Thunder"] = 3,
+	-- mobility
+	["Lightning Assault"] = 2,
+	["Lightning Cloak"] = 3,
+	-- support
+	["Lightning Stream"] = 1,
+	["Spark Swap"] = 3,
+	
+	-- frostdraw
+	-- combat
+	["Ice Spikes"] = 0,
+	["Ice Beam"] = 0,
+	["Frost Grab"] = 0,
+	["Warden's Blades"] = 0,
+	["Frozen Servants"] = 0,
+	["Ice Daggers"] = 1,
+	["Ice Blade"] = 1,
+	["Ice Chain"] = 1,
+	["Ice Eruption"] = 1,
+	["Ice Forge"] = 1,
+	["Ice Smash"] = 1,
+	["Ice Lance"] = 2,
+	["Ice Fissure"] = 3,
+	-- mobility
+	["Glacial Arc"] = 1,
+	["Ice Skate"] = 3,
+	-- support
+	["Iceberg"] = 3,
+	
+	-- galebreathe
+	["Wind Blade"] = 0,
+	["Air Force"] = 0,
+	["Tornado Kick"] = 0,
+	["Gale Lunge"] = 1,
+	["Tornado"] = 1,
+	["Heavenly Wind"] = 1,
+	["Galetrap"] = 1,
+	["Gale Punch"] = 1,
+	["Champion's Whirlthrow"] = 2,
+	["Rising Wind"] = 2,
+	["Wind Gun"] = 2,
+	["Wind Carve"] = 2,
+	["Astral Wind"] = 3,
+	-- mobility
+	-- support
+	["Gale Wisp"] = 2,
+	
+	-- shadowcast
+	-- combat
+	["Dark Blade"] = 0,
+	["Shadow Gun"] = 0,
+	["Clutching Shadow"] = 0,
+	["Shadow Chains"] = 1,
+	["Shadow Eruption"] = 1,
+	["Shadow Seekers"] = 1,
+	["Encircle"] = 1,
+	["Shadow Roar"] = 2,
+	["Rising Shadow"] = 2,
+	["Shadow Meteors"] = 2,
+	["Shade Bringer"] = 3,
+	["Eclipse Kick"] = 3,
+	["Shadow Vortex"] = 3,
+	-- mobility
+	["Shadow Assault"] = 2,
+	-- support
+	["Shade Devour"] = 1,
+	
+	-- attunement-less
+	-- strength
+	["Strong Left"] = 0,
+	["Rapid Punches"] = 1,
+	["Strong Leap"] = 0,
+	["Tacet Drop Kick"] = 0,
+	-- fortitude
+	["Rally"] = 0,
+	["Reinforce"] = 0,
+	["Brace"] = 0,
+	["Shoulder Bash"] = 0,
+	-- agility
+	["Revenge"] = 0,
+	["Dash"] = 0,
+	["Adrenaline Surge"] = 0,
+	-- intelligence
+	["Summon Cauldron"] = 0,
+	["Prediction"] = 0,
+	-- willpower
+	["Gaze"] = 0,
+	["Glare"] = 0,
+	["Exhaustion Strike"] = 0,
+	-- charisma
+	["Taunt"] = 0,
+	["Sing"] = 0,
+	["Disguise"] = 0,
+	-- weapon: light
+	["Rapid Slashes"] = 0,
+	-- weapon: medium
+	["Master's Flourish"] = 0,
+	["Prominence Draw"] = 0,
+	-- weapon: heavy
+	["Pressure Blast"] = 0,
+	["Punishment"] = 0,
+	
+	-- monster
+	["Enforcer Pull"] = 2,
+	["Beast Burrow"] = 2,
+	["Coral Spear"] = 2,
+	["Dread Breath"] = 2,
+	["Brachial Spear"] = 2,
+	["Mecha Gatling"] = 2,
+	
+	-- oath
+	-- blindseer
+	["Mindsoothe"] = 0,
+	["Tranquil Circle"] = 0,
+	["Sightless Beam"] = 0,
+	-- visionshaper
+	["Illusory Servants"] = 0,
+	["Illusionary Realm"] = 0,
+	["Illusionary Counter"] = 0,
+	-- linkstrider
+	["Symbiotic Sustain"] = 0,
+	["Parasitic Leech"] = 0,
+	-- starkindred
+	["Ascension"] = 0,
+	["Celestial Assault"] = 0,
+	["Sinister Halo"] = 0,
+	-- arcwarder
+	["Arc Suit"] = 0,
+	["Arc Beam"] = 0,
+	["Arc Wave"] = 0,
+	-- dawnwalker
+	["Blinding Dawn"] = 3,
+	["Radiant Kick"] = 3,
+	-- contractor
+	["Judgement"] = 0,
+	["Lords Slice"] = 0,
+	["Equalizer"] = 0,
+}
 local elementColors = {
 	Wind = Color3.fromRGB(0,255,0),
 	Ice = Color3.fromRGB(0,255,255),
@@ -73,7 +246,7 @@ screenGui.ResetOnSpawn = false
 screenGui.ZIndexBehavior = Enum.ZIndexBehavior.Global
 shared.SC_UI = screenGui
 
-local function newSpellSign(text, element)
+local function newSpellSign(text, element, stars)
 	local mainFrame = Instance.new("Frame")
 	local line = Instance.new("Frame")
 	local line2 = Instance.new("Frame")
@@ -83,6 +256,7 @@ local function newSpellSign(text, element)
 	local gradient3
 	local imageLabel = Instance.new("ImageLabel")
 	local imageLabel2 = Instance.new("ImageLabel")
+	local imageLabel3 = Instance.new("ImageLabel")
 	local textLabel = Instance.new("TextLabel")
 	local scale = Instance.new("UIScale")
 
@@ -99,18 +273,21 @@ local function newSpellSign(text, element)
 	line.BorderSizePixel = 0
 	line.Position = UDim2.new(0, 0, 1, 0)
 	line.Size = UDim2.new(1, 0, 0, -4)
+	line.ZIndex = 3
 	
 	line2.Parent = line
 	line2.BackgroundTransparency = 0.5
 	line2.BorderSizePixel = 0
 	line2.Position = UDim2.new(0, 20, 1, 4)
 	line2.Size = UDim2.new(1, -40, 0, -3)
+	line2.ZIndex = 3
 	
 	line3.Parent = line2
 	line3.BackgroundTransparency = 0.8
 	line3.BorderSizePixel = 0
 	line3.Position = UDim2.new(0, 40, 1, 3)
 	line3.Size = UDim2.new(1, -80, 0, -2)
+	line3.ZIndex = 3
 
 	gradient.Parent = line
 	gradient.Color = ColorSequence.new{
@@ -137,13 +314,17 @@ local function newSpellSign(text, element)
 
 	imageLabel.Parent = mainFrame
 	imageLabel.BackgroundTransparency = 1
-	imageLabel.Position = UDim2.new(0, -85, 1, -60)
-	imageLabel.Size = UDim2.new(0, 80, 0, 80)
+	imageLabel.Position = stars > 1 and UDim2.new(0, -85, 1, -60) or UDim2.new(0, -115, 1, -90)
+	imageLabel.Size = stars > 1 and UDim2.new(0, 80, 0, 80) or UDim2.new(0, 140, 0, 140)
 	imageLabel.Image = "rbxassetid://5304862649"
-	imageLabel.ImageColor3 = Color3.fromRGB(255,255,255)
-	imageLabel.ImageTransparency = 0.2
+	if stars > 1 then
+		imageLabel.ImageColor3 = Color3.fromRGB(255,255,255)
+	else
+		imageLabel.ImageColor3 = elementColors[element] or Color3.fromRGB(255,255,255)
+	end
+	imageLabel.ImageTransparency = stars > 0 and 0.2 or 1
 	imageLabel.ScaleType = Enum.ScaleType.Crop
-	imageLabel.ZIndex = 2
+	imageLabel.ZIndex = 3
 
 	imageLabel2.Parent = mainFrame
 	imageLabel2.BackgroundTransparency = 1
@@ -151,8 +332,17 @@ local function newSpellSign(text, element)
 	imageLabel2.Size = UDim2.new(0, 140, 0, 140)
 	imageLabel2.Image = "rbxassetid://5304862649"
 	imageLabel2.ImageColor3 = elementColors[element] or Color3.fromRGB(255,255,255)
-	imageLabel2.ImageTransparency = .5
+	imageLabel2.ImageTransparency = stars > 1 and .5 or 1
 	imageLabel2.ScaleType = Enum.ScaleType.Crop
+
+	imageLabel3.Parent = mainFrame
+	imageLabel3.BackgroundTransparency = 1
+	imageLabel3.Position = UDim2.new(0, -145, 1, -120)
+	imageLabel3.Size = UDim2.new(0, 200, 0, 200)
+	imageLabel3.Image = "rbxassetid://5304862649"
+	imageLabel3.ImageColor3 = elementColors[element] or Color3.fromRGB(255,255,255)
+	imageLabel3.ImageTransparency = stars > 2 and .8 or 1
+	imageLabel3.ScaleType = Enum.ScaleType.Crop
 
 	textLabel.Parent = mainFrame
 	textLabel.BackgroundTransparency = 1
@@ -166,6 +356,7 @@ local function newSpellSign(text, element)
 	textLabel.TextStrokeTransparency = 0
 	textLabel.TextXAlignment = Enum.TextXAlignment.Left
 	textLabel.TextYAlignment = Enum.TextYAlignment.Bottom
+	textLabel.ZIndex = 3
 
 	return {
 		mainFrame = mainFrame,
@@ -175,6 +366,7 @@ local function newSpellSign(text, element)
 		line3 = line3,
 		imageLabel = imageLabel,
 		imageLabel2 = imageLabel2,
+		imageLabel3 = imageLabel3,
 		scale = scale
 	}
 end
@@ -184,7 +376,7 @@ local function newMove(move)
 		lastSpell.mainFrame:Destroy()
 	end
 
-	local ui = newSpellSign(move.Custom and move.Type..' "'..move.Name..'"' or move.Type..' Sign "'..move.Name..'"', move.Type)
+	local ui = newSpellSign(move.Custom and move.Type..' "'..move.Name..'"' or move.Type..' Sign "'..move.Name..'"', move.Type, move.Stars)
 	lastSpell = ui
 	
 	local tweenInfoScale = TweenInfo.new(.3, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut)
@@ -205,13 +397,14 @@ local function newMove(move)
 		local increment = 100
 		local con
 		con = runService.Heartbeat:Connect(function(deltaTime)
-			if not ui.imageLabel or not ui.imageLabel2 then
+			if not ui.imageLabel or not ui.imageLabel2 or not ui.imageLabel3 then
 				con:Disconnect()
 				return
 			end
 			
 			ui.imageLabel.Rotation = ui.imageLabel.Rotation + increment * deltaTime
 			ui.imageLabel2.Rotation = -ui.imageLabel.Rotation - increment * deltaTime
+			ui.imageLabel3.Rotation = ui.imageLabel.Rotation + increment * deltaTime
 		end)
 	end)
 	-- special types
@@ -224,7 +417,9 @@ local function newMove(move)
 				local hue = (tick()+r) % t / t
 				local color = Color3.fromHSV(hue, 1, 1)
 				local colorTween = tweenService:Create(ui.imageLabel2, tweenInfoRadiant, {ImageColor3 = color})
+				local color2Tween = tweenService:Create(ui.imageLabel3, tweenInfoRadiant, {ImageColor3 = color})
 				colorTween:Play()
+				color2Tween:Play()
 				task.wait()
 			end
 		elseif move.Type == "Tacet" then
@@ -232,7 +427,9 @@ local function newMove(move)
 			while not fadeTweenPlaying and ui.imageLabel2 do
 				local transparency = ui.imageLabel2.ImageTransparency == 0.5 and .98 or 0.5
 				local murmurTween = tweenService:Create(ui.imageLabel2, tweenInfoMurmur, {ImageTransparency = transparency})
+				local murmur2Tween = tweenService:Create(ui.imageLabel3, tweenInfoMurmur, {ImageTransparency = transparency})
 				murmurTween:Play()
+				murmur2Tween:Play()
 				task.wait(.4)
 			end
 		end
@@ -243,6 +440,7 @@ local function newMove(move)
 
 	local imageColorTween = tweenService:Create(ui.imageLabel, tweenInfoImageColor, {ImageColor3 = Color3.fromRGB(0,0,0)})
 	local image2ColorTween = tweenService:Create(ui.imageLabel2, tweenInfoImageColor, {ImageColor3 = Color3.fromRGB(0,0,0)})
+	local image3ColorTween = tweenService:Create(ui.imageLabel3, tweenInfoImageColor, {ImageColor3 = Color3.fromRGB(0,0,0)})
 
 	local midTween = tweenService:Create(ui.mainFrame, tweenInfoMid, {Position = UDim2.new(0.95, 0, 0.7, 0)})
 	local finalTween = tweenService:Create(ui.mainFrame, tweenInfoFinal, {Position = UDim2.new(0.95, 0, 0.15, 0)})
@@ -250,6 +448,7 @@ local function newMove(move)
 
 	local fadeTweenImage = tweenService:Create(ui.imageLabel, tweenInfoFade, {ImageTransparency = 1})
 	local fadeTweenImage2 = tweenService:Create(ui.imageLabel2, tweenInfoFade, {ImageTransparency = 1})
+	local fadeTweenImage3 = tweenService:Create(ui.imageLabel3, tweenInfoFade, {ImageTransparency = 1})
 	local fadeTweenText = tweenService:Create(ui.textLabel, tweenInfoFade, {TextTransparency = 1, TextStrokeTransparency = 1})
 	local fadeTweenLine = tweenService:Create(ui.line, tweenInfoFade, {BackgroundTransparency = 1})
 	local fadeTweenLine2 = tweenService:Create(ui.line2, tweenInfoFade, {BackgroundTransparency = 1})
@@ -271,15 +470,23 @@ local function newMove(move)
 	task.wait(3)
 	-- fade out
 	fadeTweenPlaying = true
+	
 	scaleExitTween:Play()
+	
 	imageColorTween:Play()
 	image2ColorTween:Play()
+	image3ColorTween:Play()
+	
 	fadeTweenImage:Play()
 	fadeTweenImage2:Play()
+	fadeTweenImage3:Play()
+	
 	fadeTweenText:Play()
+	
 	fadeTweenLine:Play()
 	fadeTweenLine2:Play()
 	fadeTweenLine3:Play()
+	
 	exitTween:Play()
 end
 
@@ -295,6 +502,7 @@ local function checkTool(tool)
 				Type = customTypes[Type]
 				custom = true
 			end
+			local stars = mantraStars[tool:FindFirstChild("DefaultName").Value] and mantraStars[tool:FindFirstChild("DefaultName").Value] or 0
 
 			local con
 			con = tool.Equipped:Connect(function()
@@ -303,12 +511,12 @@ local function checkTool(tool)
 				local end_idx = string.find(str, "}}") - 1
 				local final_str = string.sub(str, start_idx, end_idx)
 				
-				if customNames[final_str] then
-					Type = customNames[final_str].Type
-					final_str = customNames[final_str].Name
+				if customNames[tool:FindFirstChild("DefaultName").Value] then
+					Type = customNames[tool:FindFirstChild("DefaultName").Value].Type
+					final_str = customNames[tool:FindFirstChild("DefaultName").Value].Name
 				end
 				
-				newMove({Type = Type, Name = final_str, Custom = custom})
+				newMove({Type = Type, Name = final_str, Stars = stars, Custom = custom})
 			end)
 			shared.SC_tools[tool.Name] = con
 		end
